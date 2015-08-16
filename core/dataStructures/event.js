03329ff90269fbb11156311016eb4309de1224a7
Boris Karbanovich <bacsus_beacks@mail.ru>
2015-08-16 14:24:34 +0300

    DataStructures.Event - implemented.

---
+++ core/dataStructures/event.js
@@ -0,0 +1,63 @@
+({
+	type: "class",
+	name: "core.dataStructures.event",
+	getter: function () {
+		"use strict";
+
+		return {
+			name: "Event",
+
+			ctor: function Event() {
+				this._handlers = [];
+			},
+
+			proto: {
+				_count: 0,
+
+				_addFromIndex: function Event__addRangeFromIndex(context, startIndex, indexes, handler, index) {
+					this.add(handler, context, startIndex + index);
+					indexes.push(startIndex + index);
+				},
+
+				addRange: function Event_addRange(handlers, context, startIndex) {
+					var indexes = [];
+					handlers.forEach(this._addFromIndex.bind(this, context, startIndex, indexes));
+					return indexes;
+				},
+
+				add: function Event_add(handler, context, index) {
+					if ("object" === typeof context) {
+						handler = handler.bind(context);
+					}
+
+					if ("number" === typeof index) {
+						this._handlers.splice(index, 0, handler);
+					} else {
+						index = this._handlers.length;
+						this._handlers[index] = handler;
+					}
+
+					return index;
+				},
+
+				remove: function Event_remove(index) {
+					this._handlers.splice(index, 1);
+				},
+
+				removeAll: function Event_removeAll() {
+					this._handlers = [];
+				},
+
+				trigger: function Event_trigger(argument) {
+					var handlers = this._handlers,
+						handlersCount = handlers.length,
+						i;
+
+					for (i = 0; i < handlersCount; i++) {
+						handlers[i](argument);
+					}
+				}
+			}
+		};
+	}
+})
